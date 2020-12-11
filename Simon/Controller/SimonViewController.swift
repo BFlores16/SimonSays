a50de6fb7b40a4d076e33dd0a24c73f364257b33
Brando <brandoflores16@gmail.com>
2020-12-10 18:19:10 -0700

    Add Simon functionality

--- Simon/Controller/SimonViewController.swift
+++ Simon/Controller/SimonViewController.swift
@@ -12,6 +12,10 @@ class SimonViewController: UIViewController {
     private var currentPlayer = 0
     private var scores = [0,0]
     
+    private var sequenceIndex = 0
+    private var colorSequence = [Int]()
+    private var colorsToTap = [Int]()
+    
     @IBOutlet var coloredButtons: [CircularButton]!
     @IBOutlet weak var actionButton: UIButton!
     @IBOutlet var playerLabels: [UILabel]!
@@ -32,19 +36,66 @@ class SimonViewController: UIViewController {
     }
     
     func createNewGame() {
-        actionButton.setTitle("Start Game", for: .normal)
+        colorSequence.removeAll()
         
+        actionButton.setTitle("Start Game", for: .normal)
+        actionButton.isEnabled = true
         for button in coloredButtons {
             button.alpha = 0.5
+            button.isEnabled = false
         }
+    }
+    
+    func addNewColor() {
+        // Generate a random number for Simon to press. The number represents
+        // the button's tag
+        colorSequence.append(Int(arc4random_uniform(UInt32(4))))
+    }
+    
+    func playSequence() {
+        if sequenceIndex < colorSequence.count {
+            flash(button: coloredButtons[colorSequence[sequenceIndex]])
+            sequenceIndex += 1
+        }
+        else {
+            colorsToTap = colorSequence
+            view.isUserInteractionEnabled = true
+            actionButton.setTitle("Tap the circles", for: .normal)
+            for button in coloredButtons {
+                button.isEnabled = true
+            }
+        }
+    }
+    
+    func flash(button: CircularButton) {
+        // Just creates the animation to flash a button
+        UIView.animate(withDuration: 0.5) {
+            button.alpha = 1.0
+            button.alpha = 0.5
+        } completion: { (Bool) in
+            self.playSequence()
+        }
+
     }
     
     @IBAction func coloredButtonsPressed(_ sender: CircularButton) {
         print("Button \(sender.tag) tapped")
     }
     @IBAction func actionButtonPressed(_ sender: UIButton) {
-        print("action button")
+        // Disable all user interaction while game loads
+        view.isUserInteractionEnabled = false
+        
+        sequenceIndex = 0
+        actionButton.setTitle("Memorize", for: .normal)
+        actionButton.isEnabled = false
+        
+        addNewColor()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+            self.playSequence()
+        }
     }
+    
     
 
 }
